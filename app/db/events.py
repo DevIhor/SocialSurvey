6e0aaf04ebbe6076743d68e8acab4a8b57173842
DevIhor <ihor.l@coaxsoft.com>
2021-02-02 23:41:52 +0200

    feat: Add app DB events Add database open and close events

---
+++ app/db/events.py
@@ -0,0 +1,19 @@
+import asyncpg
+
+from fastapi import FastAPI
+from loguru import logger
+
+from app.core.config import DATABASE_URL, MIN_CONNECTIONS_COUNT, MAX_CONNECTIONS_COUNT
+
+
+async def connect_to_db(app: FastAPI) -> None:
+    logger.info(f"Connecting to {repr(DATABASE_URL)}")
+    app.state.pool = await asyncpg.create_pool(
+        str(DATABASE_URL), min_size=MIN_CONNECTIONS_COUNT, max_size=MAX_CONNECTIONS_COUNT)
+    logger.info("Connection established")
+
+
+async def close_db_connection(app: FastAPI) -> None:
+    logger.info("Closing connection to database")
+    await app.state.pool.close()
+    logger.info("Connection closed")
